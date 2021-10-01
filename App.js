46bdd309313e873369e5701a3955a92a593eb02e
Dienvm <dienvm.pnv@gmail.com>
2021-10-02 00:46:34 +0700

    Implement drawer navigator

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { MainStackNavigator } from "./src/navigations/StackNavigator";
-import BottomTabNavigator from "./src/navigations/TabNavigator";
+// import { MainStackNavigator } from "./src/navigations/StackNavigator";
+// import BottomTabNavigator from "./src/navigations/TabNavigator";
+import DrawerNavigator from "./src/navigations/DrawerNavigator";
 
 const App = () => (
   <NavigationContainer>
-    <BottomTabNavigator />
+    <DrawerNavigator />
   </NavigationContainer>
 );
 export default App;
