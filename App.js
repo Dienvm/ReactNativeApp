8f36b710befad5f47b6a64f31a12972789c4de59
Dienvm <dienvm.pnv@gmail.com>
2021-10-02 00:41:00 +0700

    Implement bottom tabs navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStackNavigator } from "./src/navigations/StackNavigator";
+import BottomTabNavigator from "./src/navigations/TabNavigator";
 
 const App = () => (
   <NavigationContainer>
-    <MainStackNavigator />
+    <BottomTabNavigator />
   </NavigationContainer>
 );
 export default App;
---
+++ src/navigations/TabNavigator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MainStackNavigator, ContactStackNavigator } from "./StackNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => (
+  <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Screen name="HomeTab" component={MainStackNavigator} />
+    <Tab.Screen name="ContactTab" component={ContactStackNavigator} />
+  </Tab.Navigator>
+);
+
+export default BottomTabNavigator;
